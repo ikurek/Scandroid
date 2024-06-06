d2737d56e593454ca085e741222eab488ef9e09b
Igor Kurek <ikurek@users.noreply.github.com>
2024-06-06 12:18:07 +0200

    Update to Gradle 8.8 and Java 21

--- build-logic/convention/src/main/kotlin/com/ikurek/scandroid/buildlogic/common/Kotlin.kt
+++ build-logic/convention/src/main/kotlin/com/ikurek/scandroid/buildlogic/common/Kotlin.kt
@@ -20,15 +20,15 @@ internal fun Project.configureKotlin(
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
             isCoreLibraryDesugaringEnabled = true
         }
     }
 
     tasks.withType<KotlinCompile>().configureEach {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
 
