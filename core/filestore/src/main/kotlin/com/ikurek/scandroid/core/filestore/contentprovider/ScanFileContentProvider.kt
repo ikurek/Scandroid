e28dc60d17ed4c6489b789b882ee85a9446c59f9
Igor Kurek <ikurek@users.noreply.github.com>
2024-06-06 12:57:23 +0200

    Setup file provider for scan files

---
+++ core/filestore/src/main/kotlin/com/ikurek/scandroid/core/filestore/contentprovider/ScanFileContentProvider.kt
@@ -0,0 +1,6 @@
+package com.ikurek.scandroid.core.filestore.contentprovider
+
+import androidx.core.content.FileProvider
+import com.ikurek.scandroid.core.filestore.R
+
+internal class ScanFileContentProvider : FileProvider(R.xml.scan_file_paths)
