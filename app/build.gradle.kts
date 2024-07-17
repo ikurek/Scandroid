87e70519af613c0415d1dc869c4d3d5b3b9b7080
Igor Kurek <ikurek@users.noreply.github.com>
2024-07-17 20:08:24 +0200

    Update version for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
 
     defaultConfig {
         applicationId = "com.ikurek.scandroid"
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildTypes {
